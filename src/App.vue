f7c1976b19aea93dea9a40a69e046238a1ec6690
Paula Rinaldi <paula.rinaldi@radiumrocket.com>
2023-06-28 12:32:59 -0300

    Added form to add game and remove button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { watchDebounced } from "@vueuse/core";
+import { v4 as uuidv4 } from "uuid";
 
 enum GameState {
   FOR_SALE = "FOR_SALE",
@@ -12,20 +13,20 @@ interface Games {
   title: string;
   releaseDate: Date;
   genre: string;
-  price?: number;
+  price?: number | string;
   discountPercentage?: number;
   images?: {
     url: string;
     previewUrl: string;
     alt: string;
   };
-  state: GameState;
-  wishlist: boolean;
+  state?: GameState;
+  wishlist?: boolean;
 }
 
 const games = ref<Games[] | null>([
   {
-    id: "1",
+    id: uuidv4(),
     title: "League of Legends",
     releaseDate: new Date(2009, 9, 27),
     genre: "Battle arena",
@@ -33,7 +34,7 @@ const games = ref<Games[] | null>([
     wishlist: false,
   },
   {
-    id: "2",
+    id: uuidv4(),
     title: "DOTA",
     releaseDate: new Date(2013, 6, 9),
     genre: "Strategy",
@@ -41,7 +42,7 @@ const games = ref<Games[] | null>([
     wishlist: false,
   },
   {
-    id: "3",
+    id: uuidv4(),
     title: "The Sims 4",
     releaseDate: new Date(2014, 8, 2),
     genre: "Simulation",
@@ -49,7 +50,7 @@ const games = ref<Games[] | null>([
     wishlist: false,
   },
   {
-    id: "4",
+    id: uuidv4(),
     title: "Inside",
     releaseDate: new Date(2019, 7, 29),
     genre: "Platform, Puzzle, Adventure",
@@ -58,7 +59,7 @@ const games = ref<Games[] | null>([
     wishlist: false,
   },
   {
-    id: "5",
+    id: uuidv4(),
     title: "Limbo",
     releaseDate: new Date(2010, 8, 21),
     genre: "Platform, Puzzle, Adventure",
@@ -69,8 +70,8 @@ const games = ref<Games[] | null>([
 ]);
 
 const addToWishlist = (id: string) => {
-  if (games.value) {
-    const updatedGames = games.value.map((item) => {
+  if (filteredGames.value) {
+    const updatedGames = filteredGames.value.map((item) => {
       if (item.id === id) {
         item.wishlist = true;
       }
@@ -81,8 +82,8 @@ const addToWishlist = (id: string) => {
 };
 
 const buy = (id: string) => {
-  if (games.value) {
-    const updatedGames = games.value.map((item) => {
+  if (filteredGames.value) {
+    const updatedGames = filteredGames.value.map((item) => {
       if (item.id === id) {
         item.state = GameState.READY_TO_PLAY;
         item.wishlist = false;
@@ -94,6 +95,12 @@ const buy = (id: string) => {
 };
 
 let searchInputText = ref("");
+let titleInput = ref("");
+let genreInput = ref("");
+let releaseDateInput = ref("");
+let priceInput = ref("");
+
+console.log(releaseDateInput);
 
 const filteredGames = ref<Games[] | undefined | null>(games.value);
 const filterGames = () => {
@@ -105,6 +112,41 @@ const filterGames = () => {
 };
 
 watchDebounced(searchInputText, filterGames, { debounce: 500, maxWait: 1000 });
+
+const add = (game: Games) => {
+  try {
+    if (game && filteredGames.value) {
+      filteredGames.value = [
+        ...filteredGames.value,
+        {
+          id: game.id,
+          title: game.title,
+          genre: game.genre,
+          releaseDate: game.releaseDate,
+          price: game.price,
+          wishlist: false,
+          state: GameState.FOR_SALE,
+        },
+      ];
+      console.log(game);
+    }
+  } catch (err) {
+    console.log(err);
+  } finally {
+    titleInput.value = "";
+    genreInput.value = "";
+    releaseDateInput.value = "";
+    priceInput.value = "";
+  }
+};
+
+const remove = (id: string) => {
+  if (filteredGames.value) {
+    filteredGames.value = filteredGames.value.filter((game) => {
+      return game.id !== id;
+    });
+  }
+};
 </script>
 
 <template>
@@ -112,60 +154,137 @@ watchDebounced(searchInputText, filterGames, { debounce: 500, maxWait: 1000 });
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
   />
-  <div className="min-h-screen bg-slate-800">
-    <!-- <div>hola</div> -->
-    <div className="flex flex-col items-center py-10 gap-10">
-      <input
-        v-model="searchInputText"
-        type="text"
-        placeholder="Search"
-        class="input input-bordered w-full max-w-xs"
-      />
-      <div
-        v-if="searchInputText && filteredGames?.length === 0"
-        class="h-screen flex text-lg justify-center"
-      >
-        No game found
-      </div>
-      <div class="item game" v-for="game in filteredGames" :key="game.id">
-        <div class="card w-96 bg-base-100 shadow-xl">
-          <div class="card-body gap-5">
-            <div className="flex justify-between">
-              <h2 class="card-title">{{ game.title }}</h2>
-              <span>{{ game.price ? `$${game.price}` : "Free to play" }}</span>
-            </div>
-            <span>Genre: {{ game.genre }}</span>
-            <span
-              >Release date:
-              {{ game.releaseDate.toISOString().split("T")[0] }}</span
-            >
-            <div class="card-actions justify-start gap-10 mt-5">
-              <button
-                v-if="game.state === GameState.FOR_SALE"
-                class="btn btn-primary"
-                @click="buy(game.id)"
-              >
-                Buy Now
-              </button>
-              <button
-                v-else-if="game.state === GameState.READY_TO_PLAY"
-                class="btn btn-primary"
-              >
-                Play
-              </button>
-              <button
-                @click="addToWishlist(game.id)"
-                v-if="!game.wishlist && game.state === GameState.FOR_SALE"
-                class="btn btn-secondary text-xs"
-              >
-                + Wishlist
-              </button>
+  <div className="min-h-screen bg-slate-800 flex flex-col pt-5">
+    <h1 class="text-3xl self-center font-bold">Games</h1>
+    <div class="flex justify-around py-10">
+      <form @submit.prevent className="flex flex-col gap-5">
+        <h2 class="self-center text-xl font-bold">Add Game</h2>
+        <fieldset class="flex flex-col gap-2">
+          <label for="titleInput">Title *</label>
+          <input
+            v-model="titleInput"
+            name="titleInput"
+            type="text"
+            class="input input-bordered w-full max-w-xs"
+          />
+        </fieldset>
+        <fieldset class="flex flex-col gap-2">
+          <label for="genreInput">Genre *</label>
+          <input
+            name="genreInput"
+            v-model="genreInput"
+            type="text"
+            class="input input-bordered w-full max-w-xs"
+          />
+        </fieldset>
+        <fieldset class="flex flex-col gap-2">
+          <label for="releaseDateInput">Release Date *</label>
+          <input
+            v-model="releaseDateInput"
+            name="releaseDateInput"
+            type="date"
+            class="input input-bordered w-full max-w-xs"
+          />
+        </fieldset>
+        <fieldset class="flex flex-col gap-2">
+          <label for="priceInput">Price</label>
+          <input
+            name="priceInput"
+            v-model="priceInput"
+            type="number"
+            class="input input-bordered w-full max-w-xs"
+          />
+        </fieldset>
+        <button
+          type="submit"
+          class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg mt-5"
+          @click="
+            if (titleInput && genreInput && releaseDateInput) {
+              add({
+                id: uuidv4(),
+                title: titleInput,
+                genre: genreInput,
+                releaseDate: new Date(releaseDateInput),
+                price: priceInput,
+              });
+            }
+          "
+        >
+          Submit
+        </button>
+      </form>
+      <div className="flex flex-col items-center gap-10">
+        <input
+          v-model="searchInputText"
+          type="text"
+          placeholder="Search"
+          class="input input-bordered w-full max-w-xs"
+        />
+        <div
+          v-if="searchInputText && filteredGames?.length === 0"
+          class="h-screen flex text-lg justify-center"
+        >
+          No game found
+        </div>
+        <div class="item game" v-for="game in filteredGames" :key="game.id">
+          <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body gap-5">
+              <div className="flex justify-between">
+                <h2 class="card-title">{{ game.title }}</h2>
+                <button
+                  @click="remove(game.id)"
+                  class="btn btn-circle btn-xs btn-outline"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-3 w-3"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <span>Genre: {{ game.genre }}</span>
               <span
-                v-else-if="game.wishlist && game.state === GameState.FOR_SALE"
-                className="self-center text-xs"
+                >Release date:
+                {{ game.releaseDate.toISOString().split("T")[0] }}</span
               >
-                Added to wishlist
-              </span>
+              <span>{{ game.price ? `$${game.price}` : "Free to play" }}</span>
+              <div class="card-actions justify-start gap-10 mt-5">
+                <button
+                  v-if="game.state === GameState.FOR_SALE"
+                  class="btn btn-primary"
+                  @click="buy(game.id)"
+                >
+                  Buy Now
+                </button>
+                <button
+                  v-else-if="game.state === GameState.READY_TO_PLAY"
+                  class="btn btn-primary"
+                >
+                  Play
+                </button>
+                <button
+                  @click="addToWishlist(game.id)"
+                  v-if="!game.wishlist && game.state === GameState.FOR_SALE"
+                  class="btn btn-secondary text-xs"
+                >
+                  + Wishlist
+                </button>
+                <span
+                  v-else-if="game.wishlist && game.state === GameState.FOR_SALE"
+                  className="self-center text-xs"
+                >
+                  Added to wishlist
+                </span>
+              </div>
             </div>
           </div>
         </div>
